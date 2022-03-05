d970c98018ec850d754d363e875b2977c5b5fa7a
BADAM MANISRINIVASA NARAYANA <50072053+ManiSrinivasa1999@users.noreply.github.com>
2022-03-05 19:35:40 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,83 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <!-- <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar> -->
+
+    <v-main>
+      <v-data-table
+        :headers="headers"
+        :items="products"
+        :items-per-page="10"
+        class="elevation-1"
+      ></v-data-table>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+
+  components: {},
+
+  data: () => ({
+    products: [],
+    headers: [
+      {
+        text: "Symbol",
+        align: "start",
+        sortable: false,
+        value: "symbol",
+      },
+      { text: "Description", value: "description" },
+      { text: "Underlying Asset", value: "underlying_asset.symbol" },
+      // { text: "Market Price", value: "carbs" },
+    ],
+  }),
+  mounted() {
+    this.fetchDataFromApi();
+  },
+  methods: {
+    async fetchDataFromApi() {
+      try {
+        let data = await axios.get("https://api.delta.exchange/v2/products");
+        this.products = data.data["result"];
+        console.log(this.products);
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
